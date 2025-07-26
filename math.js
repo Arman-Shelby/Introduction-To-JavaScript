9726987acd147959de9ceae86dd0a728ecddecdf
Arman Mir <mirarman8583@gmail.com>
2025-07-26 21:25:51 +0600

    Maths needed for js

---
+++ math.js
@@ -0,0 +1,17 @@
+/** 
+ * *****************************************************
+ *               MATH NEEDED FOR PROGRAMMING
+ * *****************************************************
+ * 1. Number: +ve , -ve , 0 [50, -9, 0]
+ * 2. integer: 2, 88, [odd , even ]: 2, 4, 897
+ * 3. Float: 6.6 , 2.5
+ * 4. Operation: +, - , *, / 
+ * 5. More Operation: %
+ * 6. Percentage: 5%, 50 taka er 10% koto?
+ * 7. Sum: 24, 25, 27
+ * 8. Avg: 54, 87, 98
+ * 9. Max/Min: 14, 18, 16
+ * 10. Series: 
+ * 11. Area: Triangle
+ * 12. 
+ */
