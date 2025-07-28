cf39f789e9502fe5ec603ad4cc85f7f0404f37f3
Arman Mir <mirarman8583@gmail.com>
2025-07-28 08:38:47 +0600

    if lese

---
+++ if.js
@@ -0,0 +1,15 @@
+var biriyaniPrice = 250;
+if(biriyaniPrice < 300) {
+    console.log('Mama biriyani khabo')
+}
+else{
+    console.log('Cha diya biscuits khabo ')
+}
+
+var weight = 20;
+if(weight<=20){
+    console.log('I will carry it')
+}
+else{
+    console.log('I will rent a rickshaw')
+}
