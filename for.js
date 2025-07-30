f014108f25fabf42653afca11ca22d3eb64c585b
Arman Mir <mirarman8583@gmail.com>
2025-07-30 18:30:09 +0600

    for loops

---
+++ for.js
@@ -0,0 +1,9 @@
+let num = 0;
+// while (num < 5){
+//     console.log(num); // loop statement
+//     num++;  //loop variable change 
+// }
+//for(first part; second part ; third part)
+for ( let num = 0 ; num < 5 ; num++ ){
+    console.log(num)
+}
