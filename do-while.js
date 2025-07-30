e4adc55d45d4746fcc232c2770d76803400075a8
Arman Mir <mirarman8583@gmail.com>
2025-07-30 21:16:56 +0600

    do-while

---
+++ do-while.js
@@ -0,0 +1,16 @@
+//Almost use korbo nah 
+
+let i = 0;
+while(i<5){
+    console.log(i);
+    i++;
+}
+
+console.log('Do while loop: ');
+
+let n = 0;
+do{
+    console.log(n);
+    n++;
+}
+while(n<5)
