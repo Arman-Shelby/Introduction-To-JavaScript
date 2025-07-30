81995fbe8352325d9f272efdbc39a6e43944e810
Arman Mir <mirarman8583@gmail.com>
2025-07-30 13:09:09 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,14 @@
+const number = 7;
+if (number< 10) {
+    console.log('Choto ekta Number paisi');
+}
+console.log('something new is cooking ');
+
+//----------------While Loop--------------------
+let num = 0; //loop variable
+while (num<5){
+    console.log('Looooooooping', num); 
+   // num = num+1;
+    // num+=1;
+    num++;
+}
