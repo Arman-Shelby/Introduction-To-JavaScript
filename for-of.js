86fbab3a882e8e013dfa616b1ba1758b10d23739
Arman Mir <mirarman8583@gmail.com>
2025-07-30 09:42:55 +0600

    for-off

---
+++ for-of.js
@@ -0,0 +1,15 @@
+// const numbers= [ 12, 85, 15, 65, 54, 87, 54, 85, 15, 65];
+
+// console.log(numbers);
+
+// for(const num of numbers ) {
+//     console.log(num)
+// }
+
+
+const fruits  = [ 'Orange', 'Banana', 'JackFruit', 'WaterMelon', 'Kodu', 'Korola' ];
+
+for( const fruit of fruits){
+    console.log( fruit );
+    console.log('I want to eat.');
+}
---
+++ recap.js
@@ -0,0 +1,16 @@
+// const : value will not change 
+//Let : Value can change 
+//Var: We should not use it 
+
+//Variable
+const amount = 200;
+//Condition
+if ( amount> 100) {
+
+}
+else{
+
+}
+
+// Array
+const numbers = [12, 32, 34, 56];
