8424283625594d0d52123aa747f79e3b9ae4b2ab
Arman Mir <mirarman8583@gmail.com>
2025-07-30 09:01:49 +0600

    shortcut

---
+++ shorcut.js
@@ -0,0 +1,8 @@
+const isLeader = true;
+//True check korte hole value na dileo cholbe just value dilei hobe
+if(isLeader){
+    console.log('Leader asche chair char');
+}
+else{
+    console.log('Vaar mein jao bacha');
+}
